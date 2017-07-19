eed2d5f8b90a082b224599f48c230884e0a20cfd
jacob boutin <jakobboutin@jacobs-MacBook-Pro.local>
2017-07-19 16:04:04 -0600

    add storage upvote downvote

--- scripts.js
+++ scripts.js
@@ -57,30 +57,51 @@ $(".idea-section").on('click', '.upvote-btn', upvoteCard)
 
 function upvoteCard(clickedCard){
   //update card on screen
+  var cardKey = $(this).parent().data("storageId");
   var currentSpan = $(this).parent().find('span');
-  console.log(currentSpan);
   if (currentSpan.text() === "swill"){
     currentSpan.text("plausible");
+    var currentCard = getCard(cardKey);
+    currentCard.quality = "plausible";
+    setCard(currentCard);
+
+
+
     //update local storage with new card quality
   }
   else if (currentSpan.text() === "plausible"){
     currentSpan.text("genius");
+    var currentCard = getCard(cardKey);
+    currentCard.quality = "genius";
+    setCard(currentCard);
     //update local storage with new card quality
   }
 }
-
+function setCard(aCard){
+    localStorage.setItem(aCard.id, JSON.stringify(aCard));
+}
+function getCard(storageKey) {
+    return JSON.parse(localStorage.getItem(storageKey));
+}
 //event listener in jquery for downvote button
 $(".idea-section").on('click', '.downvote-btn', downvoteCard);
 
 function downvoteCard(){
   //update card on screen
+  var cardKey = $(this).parent().data("storageId");
   var currentSpan = $(this).parent().find('span');
   if (currentSpan.text() === "genius"){
     currentSpan.text("plausible");
+    var currentCard = getCard(cardKey);
+    currentCard.quality = "plausible";
+    setCard(currentCard);
     //update local storage with new card quality
   }
   else if (currentSpan.text() === "plausible"){
     currentSpan.text("swill");
+    var currentCard = getCard(cardKey);
+    currentCard.quality = "swill";
+    setCard(currentCard);
     //update local storage with the new card quality
   }
 }
@@ -98,35 +119,35 @@ function deleteCard(){
 
 
 function buildCard(currentCard){
-  var voteText = getQuality(currentCard.quality);
+  // var voteText = getQuality(currentCard.quality);
   return `<article class="card" data-storage-id="${currentCard.id}">
     <h3>${currentCard.title}</h3>
     <div class="delete-btn"></div>
     <p>${currentCard.body}</p>
     <div class="upvote-btn"></div>
     <div class="downvote-btn"></div>
-    <h5>quality: <span>${voteText}</span></h5>
+    <h5>quality: <span>${currentCard.quality}</span></h5>
     <hr>
   </article>`
 }
 
-function getQuality(qualityNumber){
-  if (qualityNumber === 0){
-    return 'swill';
-  }
-  else if (qualityNumber === 1){
-    return 'plausible';
-  }
-  else {
-    return 'genius';
-  }
-}
+// function getQuality(qualityNumber){
+//   if (qualityNumber === 0){
+//     return 'swill';
+//   }
+//   else if (qualityNumber === 1){
+//     return 'plausible';
+//   }
+//   else {
+//     return 'genius';
+//   }
+// }
 
 function IdeaCard(id, title, body) {
   this.id = id;
   this.title = title;
   this.body = body;
-  this.quality = 0; //quality ranges from 0-2
+  this.quality = "swill"; //quality ranges from 0-2
   // this.upvote = function(){
   //   this.quality < 2 ? this.quality++ : 0;
   // }
