d98afbfe09c00dc835bf4daa5d75bf190d9f6a9f
Rufus <rufusasterisk@gmail.com>
2017-07-19 11:33:06 -0600

    Comment out object functions

--- scripts.js
+++ scripts.js
@@ -115,10 +115,10 @@ function IdeaCard(id, title, body) {
   this.title = title;
   this.body = body;
   this.quality = 0; //quality ranges from 0-2
-  this.upvote = function(){
-    this.quality < 2 ? this.quality++ : 0;
-  }
-  this.downvote = function(){
-    this.quality > 0 ? this.quality-- : 0;
-  }
+  // this.upvote = function(){
+  //   this.quality < 2 ? this.quality++ : 0;
+  // }
+  // this.downvote = function(){
+  //   this.quality > 0 ? this.quality-- : 0;
+  // }
 }
